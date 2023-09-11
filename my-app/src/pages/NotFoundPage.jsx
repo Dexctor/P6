b2689b14ea8ee4c5b86f597c53b49f57f5628f44
Dexctor <contactantoinedewas@gmail.com>
2023-09-11 20:07:21 +0200

    création du projet + (accueil,a propos,404)

--- my-app/src/pages/NotFoundPage.jsx
+++ my-app/src/pages/NotFoundPage.jsx
@@ -1,12 +1,13 @@
 // NotFoundPage.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/NotFoundPage.scss';
 const NotFoundPage = () => {
   return (
-    <div>
+    <div className="container-404">
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
-      <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+      <NavLink to="/" className="backtohome">Retourner sur la page d'accueil</NavLink>
     </div>
   );
 };
